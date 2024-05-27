c00188c9b9942be907aaef9e655f9cf59aa78637
vitos63 <158137246+vitos63@users.noreply.github.com>
2024-05-27 17:41:31 +0500

    Delete modular_echo_bot directory

--- modular_echo_bot/keyboards/main_menu.py
+++
@@ -1,8 +0,0 @@
-from aiogram.types import BotCommand
-from lexicon.lexicon import MENU_COMMANDS
-
-
-async def set_main_menu(bot):
-    main_menu_commands = [BotCommand(command=key, description=value)
-                          for key, value in MENU_COMMANDS.items()]
-    await bot.set_my_commands(main_menu_commands)
